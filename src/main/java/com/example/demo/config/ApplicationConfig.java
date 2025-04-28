01746682e27cc3f9e12d6f799eb006474e782548
Reshzera <reoshiro13@gmail.com>
2025-04-28 11:07:38 -0400

    feat: add HLS protocol

--- src/main/java/com/example/demo/config/ApplicationConfig.java
+++ src/main/java/com/example/demo/config/ApplicationConfig.java
@@ -3,7 +3,7 @@ package com.example.demo.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,15 +11,13 @@ public class ApplicationConfig implements WebMvcConfigurer {
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-        configurer.mediaType("m3u8", MediaType.parseMediaType("application/vnd.apple.mpegurl"));
-        configurer.mediaType("ts", MediaType.parseMediaType("video/mp2t"));
+        configurer.mediaType("m3u8", MediaType.valueOf("application/vnd.apple.mpegurl"));
+        configurer.mediaType("ts", MediaType.valueOf("video/MP2T"));
     }
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "HEAD")
-                .maxAge(3600);
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/videos/**")
+                .addResourceLocations("classpath:/static/videos/");
     }
 }
